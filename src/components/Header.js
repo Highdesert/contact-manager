2d6750c1f78a0bfe3aaa75440b0e121e639c2e0c
Andy <a.jaxon09@gmail.com>
2019-02-11 23:04:46 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    const {branding} = props; 
+    <div>
+      <h1>{branding}</h1>
+    </div>
+  );
+};
+
+export default Header;
